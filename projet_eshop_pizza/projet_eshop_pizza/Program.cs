ffb2172d3631d7427769313b9fe4bdf9c27b7494
doan <thomas.doan@laplateforme.io>
2022-06-23 12:21:18 +0200

    rajout des ingredients

--- projet_eshop_pizza/projet_eshop_pizza/Program.cs
+++ projet_eshop_pizza/projet_eshop_pizza/Program.cs
@@ -9,12 +9,23 @@ namespace projet_eshop_pizza
        public string nom { get; init; }
         float prix { get; init; }
         bool vegetarienne { get; init; }
+        List <string> ingredients;
 
-        public Pizza(string nom, float prix, bool vegetarienne)
+        public Pizza(string nom, float prix, bool vegetarienne, List<string> ingredients)
         {
-            this.nom = nom;
-            this.prix = prix;
-            this.vegetarienne = vegetarienne;
+
+                this.nom = nom;
+                this.prix = prix;
+                this.vegetarienne = vegetarienne;
+                this.ingredients = ingredients;
+            
+
+            if (string.IsNullOrEmpty(nom))
+            {
+                nom = "pizza sans nom";
+                this.nom = nom;
+            }
+        
 
         }
 
@@ -36,11 +47,13 @@ namespace projet_eshop_pizza
             } 
             
                 Console.WriteLine($"{nomFormate} {badgeVegetarienne} - {prix}€");
+                Console.WriteLine($"{string.Join(", ", ingredients)}");
+                Console.WriteLine();
          
             
         }
 
-        private string FormaterNomDePizzaPremiereLettreMajuscule(string data_nom_pizza)
+        private static string FormaterNomDePizzaPremiereLettreMajuscule(string data_nom_pizza)
         {
             
 
@@ -48,10 +61,8 @@ namespace projet_eshop_pizza
 
            char premiereLettreMajuscule =  char.ToUpper(data_nom_pizza[0]);
 
-            string resultat = $"{premiereLettreMajuscule}{nomMinuscule[1..]}";
-
-       
-
+           string resultat = $"{premiereLettreMajuscule}{nomMinuscule[1..]}";
+             
             return resultat;
 
         }
@@ -62,17 +73,17 @@ namespace projet_eshop_pizza
 class Program
     {
 
-
         static void Main(string[] args)
     {
 
             var liste_de_pizzas = new List<Pizza>
             {
-                new Pizza("4 fromages", 8.5f, false),
-                new Pizza("SUPER Vege", 8f, true),
-                new Pizza("royAL", 9.5f, false),
-                new Pizza("soLEIL du midi", 12.5f, true)
+                new Pizza("4 fromages", 8.5f, false, new List<string>{"roquefort", "chevre", "conte", "gorgo"}),
+                new Pizza("SUPER Vege", 8f, true, new List<string>{"tomate", "salade", "haricot"}),
+                new Pizza( "royal" ,9.5f, false, new List<string>{"tomate", "jambon", "fromage"} ),
+                new Pizza("soLEIL du midi", 12.5f, true, new List<string>{"tomate", "basilic", "vinaigre", "gorgo"} ),
             };
+
 
             foreach (var pizza in liste_de_pizzas)
             {
